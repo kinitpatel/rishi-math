fa221c9f6f20fc070a98b20a62c71354f74e2924
Kinit Patel <kinit_patel@apple.com>
2021-04-08 13:41:19 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<!-- App.vue -->
+
+<v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="pt-4"
+      color="grey lighten-3"
+      permanent
+    >
+
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            MathLeague Trainer
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            boom
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+       <v-divider></v-divider>
+  
+        <v-list
+          dense
+          nav
+        >
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+            
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+  
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+
+  },
+
+  data: () => ({
+    items: [
+      { title: 'Remainders', icon: 'mdi-play-box' },
+      { title: 'Add 4 numbers', icon: 'mdi-play-box'},
+      { title: 'Add 2 numbers', icon: 'mdi-play-box' },
+      { title: 'Multiply', icon: 'mdi-play-box' }, 
+      { title: 'Compliments', icon: 'mdi-play-box' },
+    ]
+  }),
+};
+</script>
